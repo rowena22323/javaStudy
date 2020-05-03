e7f22e9e7057390d473a3a236c5ec3105e33c6ec
rowena22323 <53651175+rowena22323@users.noreply.github.com>
2020-05-03 16:56:15 +0900

    0507 배열

---
+++ 0507/Exercise5_4.java
@@ -0,0 +1,33 @@
+package studyJava.ArrayEx;
+
+public class Exercise5_4 {
+
+	public static void main(String[] args) {
+		int [][] arr = {
+				{5,5,5,5,5},
+				{10,10,10,10,10},
+				{20,20,20,20,20},
+				{30,30,30,30,30}
+		};
+		
+		int total = 0;
+		float average = 0;
+		int count = 0;
+		
+		for(int i = 0; i<arr.length;i++) {
+			for(int j=0; j<arr[i].length;j++) {
+				++count;
+				total += arr[i][j];
+			}
+			average = (float) total / count;
+		}
+		
+		System.out.println("total = "+total);
+		System.out.println("average = "+average);
+
+	}
+
+}
+
+
+//shorturl.at/btyH6
