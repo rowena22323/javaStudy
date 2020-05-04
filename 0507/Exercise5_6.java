ea0274a8d41f6dcd31d7c87dd9531777c7a2b36c
rowena22323 <53651175+rowena22323@users.noreply.github.com>
2020-05-04 18:20:06 +0900

    Add files via upload

---
+++ 0507/Exercise5_6.java
@@ -0,0 +1,20 @@
+package studyJava.ArrayEx;
+
+public class Exercise5_6 {
+
+	public static void main(String[] args) {
+		int[] coinUnit = {500, 100, 50, 10};
+		int money = 2680;
+		
+		System.out.println("money = "+money);
+		for (int i=0; i<coinUnit.length;i++) {
+			int[] coin = {};
+			coin[i] = money/coinUnit[i];
+			money = money % coinUnit[i];
+			System.out.println(coinUnit[i]+"원:"+coin[i]);
+
+		}
+
+	}
+
+}
