fc69198f1b4acab0abde4cf992705dbb34c17b25
rowena22323 <53651175+rowena22323@users.noreply.github.com>
2020-05-26 17:46:02 +0900

    Add files via upload

---
+++ ArrayDequeCollection.java
@@ -0,0 +1,18 @@
+package ttest;
+
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class ArrayDequeCollection {
+	public static void main (String[]args) {
+		Deque<String> deq = new ArrayDeque<>();
+		
+		deq.offerFirst("1.Box");
+		deq.offerFirst("2.Toy");
+		deq.offerFirst("3.Robot");
+		
+		System.out.println(deq.pollFirst());
+		System.out.println(deq.pollFirst());
+		System.out.println(deq.pollFirst());
+	}
+}
